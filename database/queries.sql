39d1587c09d94e19db524cc25dc9b15cee169c4d
florardu <andretabourian@hotmail.com>
2021-05-01 23:50:28 +0300

    Added queries.sql file

---
+++ database/queries.sql
@@ -0,0 +1,14 @@
+
+
+--------------------------------------------------------------------------------
+--
+-- Company
+--
+
+SELECT C_Registration, [Name], [Location] FROM Company OFFSET 15 LIMIT 10
+
+INSERT INTO company(Name, Location) VALUES ('sjkhgvsf', 'wghdeu')
+
+UPDATE compant set Name='jehfiuwh' WHERE TRUE
+
+-------------------------------------------------------------------------------
